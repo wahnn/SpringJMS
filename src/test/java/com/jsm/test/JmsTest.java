ef0909b0722ad48fca587e827959bc9440aa6ddf
高学勇 <wb-gxy329407@alibaba-inc.com>
2018-02-02 17:53:21 +0800

    增加测试用例

--- src/test/java/com/jsm/test/JmsTest.java
+++ src/test/java/com/jsm/test/JmsTest.java
@@ -1,7 +1,11 @@
 package com.jsm.test;
 
+import com.jms.service.CustomerService;
+import com.jms.service.ProducerService;
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javax.jms.Destination;
 
 
 public class JmsTest {
@@ -9,6 +13,13 @@ public class JmsTest {
 
     @Test
     public void producerTest(){
+
         ClassPathXmlApplicationContext springContext = new ClassPathXmlApplicationContext(new String[]{"classpath:spring-core.xml"});
+        ProducerService producerService = (ProducerService)springContext.getBean("producerService");
+        CustomerService customerService = (CustomerService)springContext.getBean("customerService");
+
+        Destination destination = (Destination)springContext.getBean("queueDestination");
+        producerService.sendMessage("测试消息队列");
+        customerService.receive(destination);
     }
 }
